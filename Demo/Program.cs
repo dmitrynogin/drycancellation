5eed4108f318ecd824985eab604a94b05f751d8f
Dmitry Nogin <dmitryn@mail.com>
2019-10-01 21:39:37 -0700

    Implement Cancellation

---
+++ Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Dry.Cancellations;
+using static System.Console;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (new Cancellation(5000))
+            {
+                Task.Run(PingAsync);
+                ReadLine();
+                Cancellation.Request();
+                ReadLine();
+            }
+        }
+
+        static async Task PingAsync()
+        {
+            try
+            {
+                while (!Cancellation.Requested)
+                {
+                    await Task.Delay(100, Cancellation.Token);
+                    WriteLine("Ping");
+                }
+
+                Cancellation.ThrowIfRequested();
+            }
+            catch(OperationCanceledException)
+            {
+                WriteLine("Ping cancelled");
+            }
+        }
+    }
+}
